17dcfa9fe55a55d0e9da12cfe57ff06cc096ca11
Albert Zhang <aalbertzhang@gmail.com>
2021-02-03 13:04:09 -0800

    add cors policy

--- src/server.js
+++ src/server.js
@@ -1,30 +1,31 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const mongoose = require("mongoose")
-const s3UploadRouter = require('./routes/s3Upload.router')
-const textMsgsRouter = require('./routes/textMsgs.router')
-const sanityCheckRouter = require('./routes/sanityCheck.router')
-
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const s3UploadRouter = require("./routes/s3Upload.router");
+const textMsgsRouter = require("./routes/textMsgs.router");
+const sanityCheckRouter = require("./routes/sanityCheck.router");
+const cors = require("cors");
 
 dotenv.config();
 
-var app = express()
+var app = express();
 
 // connect to mongodb
 mongoose.connect(process.env.MONGODB_URI, {
   useCreateIndex: true,
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // routes
-app.use('/', sanityCheckRouter);
-app.use('/api', s3UploadRouter);
-app.use('/api/textMsgs', textMsgsRouter);
+app.use("/", sanityCheckRouter);
+app.use("/api", s3UploadRouter);
+app.use("/api/textMsgs", textMsgsRouter);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
