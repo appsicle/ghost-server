03054bc6c64524c7fa21155107c753c28abf526c
Harrison <harrisonhuang00@gmail.com>
2021-02-04 20:07:37 -0800

    Add s3 upload extension for various buckets

--- src/routes/s3Upload.router.js
+++ src/routes/s3Upload.router.js
@@ -5,6 +5,7 @@ const S3UploadService = require('../services/s3Upload.service')
 router.post('/getSignedURL', async (req, res, next) => {
     // TODO: auth
     const getSignedURLDTO = req.body;
+    console.log(`Endpoint: "getSignedURL", recieved: ${JSON.stringify(getSignedURLDTO)}`)
 
     const { key, uploadURL, err } = await S3UploadService.getSignedURL(getSignedURLDTO);
 
--- src/services/s3Upload.service.js
+++ src/services/s3Upload.service.js
@@ -1,37 +1,56 @@
-const aws = require("aws-sdk")
+const aws = require("aws-sdk");
 
 module.exports = {
-    getSignedURL: async (getSignedURLDTO) => {
-        const { contentType } = getSignedURLDTO
-        const fileExtension = contentType.split(/\//)[1]
+  getSignedURL: async (getSignedURLDTO) => {
+    const { contentType, bucket } = getSignedURLDTO;
+    const fileExtension = contentType.split(/\//)[1];
 
-        aws.config.setPromisesDependency();
-        aws.config.update({
-            accessKeyId: process.env.ACCESSKEYID,
-            secretAccessKey: process.env.SECRETACCESSKEY,
-            region: process.env.REGION
-        });
+    // obfuscated allowlist
+    const bucketMap = {
+      "reviewerProfilePic": "profilePic",
+      "textMsgs": "textMsgs",
+    };
 
-        const s3 = new aws.S3();
-        const URL_EXPIRATION_SECONDS = 300;
-        const randomID = parseInt(Math.random() * 10000000);
-        const key = `${randomID}.${fileExtension}`;
-
-        // Get signed URL from S3
-        const s3Params = {
-            Bucket: process.env.BUCKET_NAME,
-            Key: `textMsgs/${key}`,
-            Expires: URL_EXPIRATION_SECONDS,
-            ContentType: `${contentType}`,   //TODO: dynamically accept png and jpeg
-            ACL:'public-read'
-        };
-
-        try {
-            const uploadURL = s3.getSignedUrl('putObject', s3Params);
-            return { uploadURL, key: `textMsgs/${key}` };
-        } catch (err) {
-            console.log(err)
-            return { err };
-        }
+    if (!bucketMap[bucket]) {
+      console.log(
+        `Include bucket, inputed: ${bucket}, expected ${Object.keys(bucketMap).join(
+          ","
+        )}`
+      );
+      return {
+        err: `Include bucket, inputed: ${bucket}, expected ${Object.keys(bucketMap).join(
+          ","
+        )}`,
+      };
     }
-}
+
+    aws.config.setPromisesDependency();
+    aws.config.update({
+      accessKeyId: process.env.ACCESSKEYID,
+      secretAccessKey: process.env.SECRETACCESSKEY,
+      region: process.env.REGION,
+    });
+
+    const s3 = new aws.S3();
+    const URL_EXPIRATION_SECONDS = 300;
+    const randomID = parseInt(Math.random() * 10000000);
+    const key = `${randomID}.${fileExtension}`;
+
+    // Get signed URL from S3
+    const s3Params = {
+      Bucket: process.env.BUCKET_NAME,
+      Key: `${bucketMap[bucket]}/${key}`,
+      Expires: URL_EXPIRATION_SECONDS,
+      ContentType: `${contentType}`, //TODO: dynamically accept png and jpeg
+      ACL: "public-read",
+    };
+
+    try {
+      const uploadURL = s3.getSignedUrl("putObject", s3Params);
+      return { uploadURL, key: `${bucketMap[bucket]}/${key}` };
+    } catch (err) {
+      console.log(err);
+      return { err };
+    }
+  },
+};
