aeb9a7c530d860658886993fcad8552591d7b328
Albert Zhang <aalbertzhang@gmail.com>
2021-02-05 20:53:39 -0800

    add user to table upon signin

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -1,10 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    userId: String,
-    name: String,
-    email: String,
-    role: String
+  userId: String,
+  googleId: String,
+  name: String,
+  email: String,
+  role: String,
 });
 
 const userModel = mongoose.model('user', userSchema);
