b46c04f1bf1ee354e0ab75881ebbeeffb886a2c7
Harrison <harrisonhuang00@gmail.com>
2021-02-06 02:42:14 -0800

    Allow cross site cookies

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-const express = require("express");
-require("dotenv").config();
-const mongoose = require("mongoose");
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-const sessionMiddleware = require("./middleware/session.middleware")
+const sessionMiddleware = require('./middleware/session.middleware');
 
-const s3UploadRouter = require("./routes/s3Upload.router");
-const textMsgsRouter = require("./routes/textMsgs.router");
-const sanityCheckRouter = require("./routes/sanityCheck.router");
-const userRouter = require('./routes/user.router')
+const s3UploadRouter = require('./routes/s3Upload.router');
+const textMsgsRouter = require('./routes/textMsgs.router');
+const sanityCheckRouter = require('./routes/sanityCheck.router');
+const userRouter = require('./routes/user.router');
 const authRouter = require('./routes/auth.router');
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 var app = express();
 
@@ -24,16 +24,22 @@ mongoose.connect(process.env.MONGODB_URI, {
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(bodyParser.json())
-app.use(sessionMiddleware.session)
+app.use(
+  cors({
+    origin: 'localhost:3000',
+    credentials: true,
+    exposedHeaders: ['set-cookie'],
+  }),
+);
+app.use(bodyParser.json());
+app.use(sessionMiddleware.session);
 
 // routes
-app.use("/", sanityCheckRouter);
-app.use("/api", s3UploadRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/textMsgs", textMsgsRouter);
-app.use("/api/user", userRouter);
+app.use('/', sanityCheckRouter);
+app.use('/api', s3UploadRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/textMsgs', textMsgsRouter);
+app.use('/api/user', userRouter);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
