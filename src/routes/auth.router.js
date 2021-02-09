7428d210daa15ecc128fb5581bc5722f2f132194
Albert Zhang <aalbertzhang@gmail.com>
2021-02-08 16:56:57 -0800

    add new endpoint for signup (#4)

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -6,13 +6,50 @@ const sessionService = require('../services/session.service');
 
 router.post('/googleSignin', async (req, res) => {
   const token = req.body.idToken; // TODO: send this over header instead
-  const role = req.body.desiredRole;
 
-  console.log("googlesignin", req.body)
+  console.log('googlesignin', req.body);
 
   // verify token
   const { sub: googleId, name, email } = await authService.verifyToken(token);
-  console.log("verify token", googleId)
+  console.log('verify token', googleId);
+
+  // fetch user
+  let user;
+  const { retrievedUser, err } = await userService.retrieveWithGoogleId(
+    googleId,
+  );
+
+  if (err) {
+    res.status(500).json({ err: `failed to fetch user: ${err}` });
+  } else {
+    console.log('user found');
+  }
+  user = retrievedUser;
+
+  if (!retrievedUser) {
+    res.status(500).json({ err: `user does not exist: ${err}` });
+  }
+
+  // attach user to session
+  sessionService.attachUser(req.session, user);
+  console.log('session attached');
+
+  res.status(200).json({
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  });
+});
+
+router.post('/googleSignup', async (req, res) => {
+  const token = req.body.idToken; // TODO: send this over header instead
+  const role = req.body.desiredRole;
+
+  console.log('googlesignin', req.body);
+
+  // verify token
+  const { sub: googleId, name, email } = await authService.verifyToken(token);
+  console.log('verify token', googleId);
 
   // fetch user
   let user;
@@ -21,32 +58,36 @@ router.post('/googleSignin', async (req, res) => {
   );
   if (err) {
     res.status(500).json({ err: `failed to fetch user: ${err}` });
-  }else{
-    console.log("user found")
+  } else {
+    console.log('user found');
   }
-  user = retrievedUser;
 
   // create user if DNE
-  if (!retrievedUser) {
-    const { retrievedUser, err } = userService.createWithGoogle({
+  if (retrievedUser) {
+    res.status(500).json({ err: `user already exists: ${err}` });
+  } else {
+    const { user, err } = userService.createWithGoogle({
       googleId,
       name,
       email,
-      role
+      role,
     });
     if (err) {
       res.status(500).json({ err: `failed to create user: ${err}` });
-    }else{
-      console.log("user created")
+    } else {
+      console.log('user created');
+      console.log(user);
+      res.status(200).json({
+        // name: user.name, // what is user here?
+        // email: user.email,
+        role: user.role,
+      });
     }
-    user = retrievedUser;
   }
 
   // attach user to session
   sessionService.attachUser(req.session, user);
-  console.log("session attached")
-
-  res.status(200).json("lgtm");
+  console.log('session attached');
 });
 
 module.exports = router;
