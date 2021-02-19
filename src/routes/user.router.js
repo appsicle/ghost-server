abf5d45af6e88f90a91f714417d4d688fa8e6ee9
Harrison <harrisonhuang00@gmail.com>
2021-02-18 16:01:20 -0800

    Add useProfile endpoint

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,44 +1,47 @@
-var express = require('express')
-var router = express.Router()
-const UserService = require('../services/user.service')
+var express = require('express');
+var router = express.Router();
+const UserService = require('../services/user.service');
 
-const { wrapAsync } = require('../middleware/errorHandler.middleware')
+const { wrapAsync } = require('../middleware/errorHandler.middleware');
 const { body, custom } = require('express-validator');
-const { validate, isObjectId } = require('../middleware/expressValidator.middleware')
+const {
+  validate,
+  isObjectId,
+} = require('../middleware/expressValidator.middleware');
 
-const { isLoggedIn } = require('../middleware/auth.middleware')
+const { isLoggedIn } = require('../middleware/auth.middleware');
 
-router.post('/retrieve',
+router.post(
+  '/retrieve',
   isLoggedIn,
   validate([
-    body("userId")
-      .exists().withMessage("required").bail()
-      .custom(isObjectId),
+    body('userId').exists().withMessage('required').bail().custom(isObjectId),
   ]),
   wrapAsync(async (req, res) => {
     const { userId } = req.body;
 
-    console.log(`Endpoint: "user/retrieve", recieved: ${JSON.stringify(req.body)}`)
+    console.log(
+      `Endpoint: "user/retrieve", recieved: ${JSON.stringify(req.body)}`,
+    );
 
     const retrievedUser = await UserService.retrieve(userId);
     //TODO: dne considered an error?
 
     return res.json({ retrievedUser });
-  }))
+  }),
+);
 
-// TODO: May need updating
-router.get('/role',
-  isLoggedIn,
-  (req, res) => {
-    if (!req.session.user || !req.session.user.role) {
-      console.log('sending visitor');
-      console.log(req.session.user);
-      res.send('VISITOR');
-    } else {
-      console.log('sending not visitor');
-      console.log(req.session);
-      res.send(req.session.user.role);
-    }
-  })
+router.get('/getProfile', isLoggedIn, (req, res) => {
+  if (!req.session.user) {
+    console.log('sending visitor');
+    throw new NoAccessError('No account found');
+  }
+  console.log('profile found');
+  res.send({
+    role: req.session.user.role,
+    name: req.session.user.name,
+    profilePic: req.session.user.profilePic,
+  });
+});
 
-module.exports = router
+module.exports = router;
